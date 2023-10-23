f7eced4fa1c48eab41e92e0398940272d8035e69
Lucas Mendes <lucas.lmp77@gmail.com>
2023-10-23 14:13:18 -0300

    Alterações no disign da página de registrar usuário

--- src/Pages/Register/index.tsx
+++ src/Pages/Register/index.tsx
@@ -4,41 +4,36 @@ import { NavLink } from "react-router-dom"
 
 export const Register = () => {
     return (
-        <section className={styles.conteiner}>
-            <div className={styles.conteinerHome}>
-                <div className={styles.firstContent}>
-                    <div className={styles.secondColum}>
-                        <h2 className={styles.title2}>Crie Sua Conta</h2>
-                        <div className={styles.form}>
-                            <div className={styles.inputs}> 
-                                <label>Login</label>
-                                <input type="text"/>
-                            </div>
-                            <div className={styles.inputs}> 
-                                <label>Email</label>
-                                <input type="text"/>
-                            </div>
-                            <div className={styles.inputs}>
-                                <label>Senha</label>
-                                <input type="password"/>
-                            </div>
-                            <div className={styles.inputs}>
-                                <label>Confirmar Senha</label>
-                                <input type="password"/>
-                            </div>
-                        </div>
-                        <button className={styles.butt2}>CADASTRE-SE</button>
+        <section className={styles.section}>
+            <div className={styles.container}>
+                <form className={styles.form}>
+                    <h2 className={styles.title}>Crie Sua Conta</h2>
+                    <div className={styles.inputs}>
+                        <label>Username</label>
+                        <input type="text" />
                     </div>
-                    <div className={styles.firstColum}>
-                        <h2 className={styles.title}>Possui uma conta ?</h2>
-                        <p className={styles.description}>Crie sua conta para embarcar em uma jornada épica.</p>
-                        <p className={styles.description}>Caso tenha uma conta, faça o login abaixo</p>
-                        <NavLink to={`/Login`} className={styles.butt_fat}>
-                            <button className={styles.butt}>ENTRAR</button>
-                        </NavLink>
+                    <div className={styles.inputs}>
+                        <label>Email</label>
+                        <input type="text" />
                     </div>
-                </div>
+                    <div className={styles.inputs}>
+                        <label>Senha</label>
+                        <input type="password" />
+                    </div>
+                    <div className={styles.inputs}>
+                        <label>Confirmar Senha</label>
+                        <input type="password" />
+                    </div>
+
+                    <button className={styles.butt}>Cadastre-se</button>
+                </form>
+                <footer className={styles.footer}>
+                    <p className={styles.text}> Já possui uma conta ?</p>
+                    <NavLink to={`/`}>
+                        <button className={styles.butt}>Entrar</button>
+                    </NavLink>
+                </footer>
             </div>
-        </section>
+        </section >
     )
 } 
