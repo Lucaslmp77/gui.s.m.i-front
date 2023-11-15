b46b4ffcd1e34410f8e6f5fbed112645b9c1f57b
Joao Schulz <joaoguilherme.da@gmail.com>
2023-11-15 19:41:38 -0300

    continuo tentando colocar essa merda

--- src/components/dice/index.tsx
+++ src/components/dice/index.tsx
@@ -2,8 +2,8 @@ import { Text } from '@react-three/drei';
 import styles from './styles.module.css';
 
 import { useRef } from "react";
-import { Canvas, useFrame } from "react-three-fiber";
-import { Mesh } from "three";
+import { Canvas, useFrame, useLoader } from "react-three-fiber";
+import { Mesh, TextureLoader } from "three";
 
 interface CubeGeoProps {
   position: [number, number, number];
@@ -12,7 +12,7 @@ interface CubeGeoProps {
   number: [number, number, number, number, number, number]
 }
 
-const CubeGeo: React.FC<CubeGeoProps> = ({ position, size, color, number }) => {
+const CubeGeo: React.FC<CubeGeoProps> = ({ position, size }) => {
 
   const meshRef = useRef<Mesh>(null);
 
@@ -20,19 +20,25 @@ const CubeGeo: React.FC<CubeGeoProps> = ({ position, size, color, number }) => {
     if (!meshRef.current){
       return;
     }
-    meshRef.current.rotation.x += 0.01;
+    meshRef.current.rotation.x += 0.1;
     meshRef.current.rotation.y += 0.01;
   });
 
+  const faces = [
+    useLoader(TextureLoader, "src/assets/SideDice/numero-1.png"),
+    useLoader(TextureLoader, "src/assets/SideDice/numero-2.png"),
+    useLoader(TextureLoader, "src/assets/SideDice/numero-3.png"),
+    useLoader(TextureLoader, "src/assets/SideDice/numero-4.png"),
+    useLoader(TextureLoader, "src/assets/SideDice/numero-5.png"),
+    useLoader(TextureLoader, "src/assets/SideDice/numero-6.png"),
+  ]
+
   return(
     <mesh ref={meshRef} position={position}>
-      <boxGeometry attach="geometry" args={size}/>
-      <meshStandardMaterial attach="material" />
-      <meshStandardMaterial attach="material" />
-      <meshStandardMaterial attach="material" />
-      <meshStandardMaterial attach="material" />
-      <meshStandardMaterial attach="material" />
-      <meshStandardMaterial attach="material" />
+      <boxGeometry attach="geometry" args={size} />
+      {faces.map((texture, index) => (
+        <meshStandardMaterial key={index} map={texture} />
+      ))}
     </mesh>
   )
 }
@@ -41,10 +47,6 @@ export default function Dice(){
   return (
     <section className={styles.container}>
       <Canvas>
-        <directionalLight 
-          position={[0,3,0]}
-          intensity={10}
-        />
         <ambientLight />
         <pointLight position={[10, 10, 10]}/>
         <CubeGeo position={[0,1,0]} color={"green"} size={[1,1,1]}/>
