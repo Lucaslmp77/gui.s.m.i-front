3bdd5d7660751342d0eb842cf6f72dfb53b51f7f
Joao Schulz <joaoguilherme.da@gmail.com>
2023-11-15 21:34:54 -0300

    finally i managed to put the number on the face of the dice

--- src/components/dice/index.tsx
+++ src/components/dice/index.tsx
@@ -1,56 +1,63 @@
-import { Text } from '@react-three/drei';
 import styles from './styles.module.css';
 
+import numero1 from "../../assets/SideDice/numero-1.png";
+import numero2 from "../../assets/SideDice/numero-2.png";
+import numero3 from "../../assets/SideDice/numero-3.png";
+import numero4 from "../../assets/SideDice/numero-4.png";
+import numero5 from "../../assets/SideDice/numero-5.png";
+import numero6 from "../../assets/SideDice/numero-6.png";
+
 import { useRef } from "react";
-import { Canvas, useFrame, useLoader } from "react-three-fiber";
+import { Canvas } from '@react-three/fiber';
+import { useFrame } from '@react-three/fiber';
+import { useLoader } from '@react-three/fiber';
 import { Mesh, TextureLoader } from "three";
 
 interface CubeGeoProps {
   position: [number, number, number];
   size: [number, number, number];
-  color: string;
-  number: [number, number, number, number, number, number]
 }
 
 const CubeGeo: React.FC<CubeGeoProps> = ({ position, size }) => {
-
   const meshRef = useRef<Mesh>(null);
 
+  const textures = [
+    useLoader(TextureLoader, numero1),
+    useLoader(TextureLoader, numero2),
+    useLoader(TextureLoader, numero3),
+    useLoader(TextureLoader, numero4),
+    useLoader(TextureLoader, numero5),
+    useLoader(TextureLoader, numero6),
+  ];
+
+  console.log('Textures:', textures);
+
   useFrame(() => {
-    if (!meshRef.current){
-      return;
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.05;
+      meshRef.current.rotation.y += 0.05;
     }
-    meshRef.current.rotation.x += 0.1;
-    meshRef.current.rotation.y += 0.01;
   });
 
-  const faces = [
-    useLoader(TextureLoader, "src/assets/SideDice/numero-1.png"),
-    useLoader(TextureLoader, "src/assets/SideDice/numero-2.png"),
-    useLoader(TextureLoader, "src/assets/SideDice/numero-3.png"),
-    useLoader(TextureLoader, "src/assets/SideDice/numero-4.png"),
-    useLoader(TextureLoader, "src/assets/SideDice/numero-5.png"),
-    useLoader(TextureLoader, "src/assets/SideDice/numero-6.png"),
-  ]
-
-  return(
+  return (
     <mesh ref={meshRef} position={position}>
-      <boxGeometry attach="geometry" args={size} />
-      {faces.map((texture, index) => (
-        <meshStandardMaterial key={index} map={texture} />
-      ))}
-    </mesh>
-  )
-}
+    <boxGeometry attach="geometry" args={size} />
+    {[0, 1, 2, 3, 4, 5].map((index) => (
+      <meshStandardMaterial key={index} map={textures[index]} attach={`material-${index}`} />
+    ))}
+  </mesh>
+  );
+};
 
-export default function Dice(){
+
+export default function Dice() {
   return (
     <section className={styles.container}>
       <Canvas>
         <ambientLight />
-        <pointLight position={[10, 10, 10]}/>
-        <CubeGeo position={[0,1,0]} color={"green"} size={[1,1,1]}/>
+        <pointLight position={[10, 10, 10]} />
+        <CubeGeo position={[0, 1, 0]} size={[1, 1, 1]} />
       </Canvas>
     </section>
-  )
+  );
 }
