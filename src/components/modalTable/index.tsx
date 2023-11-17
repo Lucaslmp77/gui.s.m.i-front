aeea74cc46f9c61f92c849591100d905e35c8273
Lucas Mendes <lucas.lmp77@gmail.com>
2023-11-17 20:23:00 -0300

    Validação dos campos da modal funcionando

--- src/components/modalTable/index.tsx
+++ src/components/modalTable/index.tsx
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 import { RpgGameClient } from '../../client/rpg-game.client.ts';
 import styles from './styles.module.css';
 import { BsXLg } from "react-icons/bs";
+import { RpgGameData } from '../../models/rpg-game-data.ts';
 
 interface CreateTableModalProps {
     isOpen: boolean;
@@ -10,54 +11,114 @@ interface CreateTableModalProps {
 }
 
 const ModalTable: React.FC<CreateTableModalProps> = ({ isOpen, onRequestClose }) => {
-    const [errorMessages, setErrorMessages] = useState<{ [key: string]: string }>({});
-    const rpgGameClient = new RpgGameClient();
-    const [rpgGame, setRpgGame] = useState({ name: '', description: '' });
+    const [formData, setFormData] = useState<any>({
+        name: "",
+        description: "",
+    });
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        setRpgGame({ ...rpgGame, [event.target.name]: event.target.value });
+    const [errors, setErrors] = useState({
+        name: "",
+        description: "",
+    });
+
+    const [successMessage, setSuccessMessage] = useState<string | null>(null);
+
+    const clearErrors = (field: string) => {
+        setErrors((prevErrors) => ({
+            ...prevErrors,
+            [field]: "",
+        }));
     };
 
-    const handleBlur = (event: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        if (event.target.name === 'name') {
-            if (!event.target.value.trim() || event.target.value.length > 25) {
-                setErrorMessages({ ...errorMessages, name: 'O nome da mesa deve ter entre 1 e 25 caracteres.' });
-            } else {
-                setErrorMessages({ ...errorMessages, name: '' });
-            }
-        } else if (event.target.name === 'description') {
-            if (event.target.value.length > 300) {
-                setErrorMessages({ ...errorMessages, description: 'A descrição da mesa não pode ter mais de 300 caracteres.' });
-            } else {
-                setErrorMessages({ ...errorMessages, description: '' });
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+        clearErrors(name);
+    };
+
+    const handleBlur = async (e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        if (name === "name" || name === "description") {
+            const isValid = await validateField(name, value);
+            if (!isValid) {
+                e.preventDefault();
             }
         }
     };
+
+    const validateField = async (name: string, value: string) => {
+        let isValid = true;
+
+        if (name === "name") {
+            if (value.length === 0) {
+                setErrors((prevErrors) => ({
+                    ...prevErrors,
+                    name: "Nome da mesa é obrigatório",
+                }));
+                isValid = false;
+            } else if (value.length < 3 || value.length > 25) {
+                setErrors((prevErrors) => ({
+                    ...prevErrors,
+                    name: "Nome da mesa deve ter entre 3 e 25 caracteres",
+                }));
+                isValid = false;
+            }
+        }
+
+        if (name === "description") {
+            if (value.length === 0) {
+                setErrors((prevErrors) => ({
+                    ...prevErrors,
+                    description: "Descrição da mesa é obrigatória",
+                }));
+                isValid = false;
+            } else if (value.length < 5 || value.length > 300) {
+                setErrors((prevErrors) => ({
+                    ...prevErrors,
+                    description: "Descrição deve ter entre 5 e 300 caracteres",
+                }));
+                isValid = false;
+            }
+        }
+
+        return isValid;
+    };
+
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-
-        // Clear previous error messages
-        setErrorMessages({});
-
-        // Validar campos no submit
-        if (!rpgGame.name.trim() || rpgGame.name.length > 25) {
-            setErrorMessages({ ...errorMessages, name: 'O nome da mesa deve ter entre 1 e 25 caracteres.' });
-            return;
-        }
-
-        if (rpgGame.description.length > 300) {
-            setErrorMessages({ ...errorMessages, description: 'A descrição da mesa não pode ter mais de 300 caracteres.' });
-            return;
-        }
-
         try {
-            await rpgGameClient.save(rpgGame);
-            setErrorMessages({ ...errorMessages, common: 'Sala criada com sucesso' });
+            const isFormValid = await validateForm();
+
+            if (isFormValid) {
+                const rpgClient = new RpgGameClient();
+                const rpg = new RpgGameData();
+
+                rpg.name = formData.name;
+                rpg.description = formData.description;
+
+                await rpgClient.save(rpg);
+                setSuccessMessage("Mesa cadastrada com sucesso");
+                setFormData({ name: "", description: "" });
+
+                setTimeout(() => {
+                    setSuccessMessage(null);
+                }, 5000);
+            }
         } catch (error) {
-            console.log(error);
-            setErrorMessages({ ...errorMessages, common: 'Erro ao criar a sala. Por favor, tente novamente.' });
+            console.error("Erro ao cadastrar mesa:", error);
         }
+    };
+
+    const validateForm = async () => {
+        let isValid = true;
+        const fieldNames = ["name", "description"];
+
+        for (const name of fieldNames) {
+            isValid = await validateField(name, formData[name]) && isValid;
+        }
+
+        return isValid;
     };
 
 
@@ -69,29 +130,30 @@ const ModalTable: React.FC<CreateTableModalProps> = ({ isOpen, onRequestClose })
             contentLabel="Criar Mesa Modal"
         >
             <div className={styles.formContainer}>
+                {successMessage && (
+                    <div className={styles.successMessage}>{successMessage}</div>
+                )}
                 <form className={styles.formModal} onSubmit={handleSubmit}>
                     <label htmlFor="tableName">Nome da Mesa</label>
                     <input
                         type="text"
-                        id="tableName"
                         name="name"
-                        value={rpgGame.name}
+                        value={formData.name}
                         onChange={handleChange}
                         onBlur={handleBlur}
                     />
-                    {errorMessages.name && <p style={{ color: 'red' }}>{errorMessages.name}</p>}
+
+                    {errors.name && <div className={styles.error}>{errors.name}</div>}
 
                     <label htmlFor="tableDescription">Descrição</label>
                     <textarea
-                        id="tableDescription"
                         name="description"
-                        value={rpgGame.description}
+                        value={formData.description}
                         onChange={handleChange}
                         onBlur={handleBlur}
                     />
-                    {errorMessages.description && <p style={{ color: 'red' }}>{errorMessages.description}</p>}
 
-                    {errorMessages.common && <p style={{ color: 'red' }}>{errorMessages.common}</p>}
+                    {errors.description && <div className={styles.error}>{errors.description}</div>}
 
                     <button type="submit">Criar Mesa</button>
                 </form>
