d4d02afd84df71fe1a94b512daaa63814a262361
Joao Schulz <joaoguilherme.da@gmail.com>
2023-11-18 15:42:13 -0300

    first commit

---
+++ src/components/modifiers/index.tsx
@@ -0,0 +1,12 @@
+import ReactModal from 'react-modal'
+import styles from './styles.module.css'
+
+const 
+
+export const modifiers = () => {
+    return(
+        <ReactModal 
+            isOpen = {true}
+            onRequestClose={true}
+    )
+}
