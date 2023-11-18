426930e9a4c9e84e750f99b6b80e52e8f4bb9949
Allyson <jandreyallyson@gmail.com>
2023-11-18 18:25:47 -0300

    criando client das regras do jogo

---
+++ src/client/rpgGameRules.client.ts
@@ -0,0 +1,59 @@
+import axios, { AxiosInstance } from "axios";
+import { GameRules } from "../models/gameRules";
+
+export class RpgGameRulesClient {
+    private axiosClient: AxiosInstance;
+
+    constructor() {
+        this.axiosClient = axios.create({
+            baseURL: 'http://localhost:3333/api/rpg-rules',
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+            }
+        });
+    }
+
+    public async save(rules: GameRules): Promise<GameRules> {
+        try {
+            return (await this.axiosClient.post('', rules)).data
+        }
+        catch (error: any) {
+            return Promise.reject(error.response)
+        }
+    }
+
+    public async findAll(page: number): Promise<GameRules[]> {
+        try {
+            return (await this.axiosClient.get(`?page=${page}`,)).data
+        }
+        catch (error: any) {
+            return Promise.reject(error.response)
+        }
+    }
+
+    public async findUnique(id: string): Promise<GameRules>{
+        try {
+            return (await this.axiosClient.get<GameRules>(`/${id}`)).data
+        }
+        catch (error: any) {
+            return Promise.reject(error.response)
+        }
+    }
+
+    public async update(id: string, rpg: GameRules): Promise<GameRules> {
+        try {
+            return (await this.axiosClient.put(`/${id}`, rpg)).data;
+        } catch (error: any) {
+            return Promise.reject(error.response);
+        }
+    }
+
+    public async delete(id: string): Promise<void> {
+        try {
+            await this.axiosClient.delete(`/${id}`);
+        } catch (error: any) {
+            return Promise.reject(error.response);
+        }
+    }
+}
