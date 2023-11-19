6a238ca4cdcf79e6a259f8c927d3ce994f46e069
Allyson <jandreyallyson@gmail.com>
2023-11-19 17:19:18 -0300

    Adicionando opções de deletar regra e editar regra

--- src/components/gameRulesModal/index.tsx
+++ src/components/gameRulesModal/index.tsx
@@ -5,6 +5,8 @@ import { GameRules } from '../../models/gameRules';
 import styles from './index.module.css';
 import Modal from 'react-modal';
 import { BsXLg } from "react-icons/bs";
+import EditRuleModal from '../editGameRulesModal';
+import AddRuleModal from '../createGameRulesModal';
 
 interface RuleListModalProps {
   isOpen: boolean;
@@ -12,7 +14,10 @@ interface RuleListModalProps {
 }
 
 const RuleListModal: React.FC<RuleListModalProps> = ({ isOpen, onRequestClose }) => {
+  const [isAddRuleModalOpen, setAddRuleModalOpen] = useState(false);
   const [rules, setRules] = useState<GameRules[]>([]);
+  const [selectedRule, setSelectedRule] = useState<GameRules | null>(null);
+  const [isSecondModalOpen, setSecondModalOpen] = useState(false);
 
   useEffect(() => {
     const fetchRules = async () => {
@@ -27,9 +32,30 @@ const RuleListModal: React.FC<RuleListModalProps> = ({ isOpen, onRequestClose })
     fetchRules();
   }, []);
 
-  if (!isOpen) {
-    return null;
-  }
+  const handleRuleClick = (rule: GameRules) => {
+    if (!event || (event.target as HTMLElement).className !== styles.deleteRule) {
+      setSelectedRule(rule);
+      setSecondModalOpen(true);
+    }
+  };
+
+  const handleDeleteRule = async (rule: GameRules) => {
+    try {
+      const isConfirmed = window.confirm(`Are you sure you want to delete the rule "${rule.name}"?`);
+
+      if (isConfirmed) {
+        await new RpgGameRulesClient().delete(rule.id);
+
+        const updatedRules = rules.filter((r) => r.id !== rule.id);
+        setRules(updatedRules);
+
+        setSecondModalOpen(false);
+        setSelectedRule(null);
+      }
+    } catch (error) {
+      console.error('Error deleting the rule:', error);
+    }
+  };
 
   return (
     <Modal
@@ -48,20 +74,49 @@ const RuleListModal: React.FC<RuleListModalProps> = ({ isOpen, onRequestClose })
           border: "1px solid #333",
           background: "transparent",
         },
-      }}>
-    <div className={styles.formContainer}>
-      
-      <ul className={styles.formModal}>
-      <h2 className={styles.subTitle}>Lista de regras</h2>
-        {rules.map((rule) => (
-          <li key={rule.id} className={styles.containerInput}>
-            <strong>{rule.name}</strong>
-            <p>{rule.description}</p>
-          </li>
-        ))}
-      </ul>
-      <BsXLg className={styles.exitModal} onClick={onRequestClose} />
-    </div>
+      }}
+    >
+      <div className={styles.formContainer}>
+        <ul className={styles.formModal}>
+          <h2 className={styles.subTitle}>Lista de regras</h2>
+          {rules.map((rule) => (
+            <li key={rule.id} className={styles.containerInput} onClick={() => handleRuleClick(rule)}>
+              <strong>{rule.name}</strong>
+              <span
+                className={styles.deleteRule}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleDeleteRule(rule);
+                }}
+              >
+                x
+              </span>
+              <p>{rule.description}</p>
+            </li>
+          ))}
+          <button onClick={() => setAddRuleModalOpen(true)}>CRIAR REGRA</button>
+        </ul>
+        <BsXLg className={styles.exitModal} onClick={onRequestClose} />
+
+        {isAddRuleModalOpen && (
+          <AddRuleModal
+            isOpen={isAddRuleModalOpen}
+            onRequestClose={() => setAddRuleModalOpen(false)}
+            initialFormData={null}
+          />
+        )}
+
+        {isSecondModalOpen && (
+          <EditRuleModal
+            isOpen={isSecondModalOpen}
+            onRequestClose={() => {
+              setSecondModalOpen(false);
+              setSelectedRule(null);
+            }}
+            initialFormData={selectedRule}
+          />
+        )}
+      </div>
     </Modal>
   );
 };
